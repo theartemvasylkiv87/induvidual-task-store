8f17ff341f8fda49d89daee0773a449077dc8760
636 <jabko@macbookpro.speedport.ip>
2026-05-29 22:38:15 +0200

    cart overlay mistackes coreccted

--- src/ts/modules/cart-overlay.ts
+++ src/ts/modules/cart-overlay.ts
@@ -4,13 +4,21 @@ export function initCartOverlay() : void {
 
     const cartOverlay = document.getElementById('cartOverlay') as HTMLElement;
     const cartButton = document.getElementById('cartLink') as HTMLElement;
+    const closeButton = document.getElementById('closeCartBtn') as HTMLElement; 
     
-    cartButton.addEventListener('click', (e: Event) => {
+    cartButton.addEventListener('click', (e: Event) : void => {
         e.preventDefault();
         cartOverlay.classList.toggle('active');
     });
 
-    cartOverlay.addEventListener('click', () => {
-        cartOverlay.classList.remove('active')
+    cartOverlay.addEventListener('click', (e: Event) : void => {
+        if (e.target === cartOverlay) {
+            cartOverlay.classList.remove('active');
+        }
+    });
+
+    closeButton.addEventListener('click', (e: Event) : void => {
+        e.preventDefault();
+        cartOverlay.classList.remove('active');
     });
 }
