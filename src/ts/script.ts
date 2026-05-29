95076886389a61be17b50417c126546a31bc6a02
636 <jabko@macbookpro.speedport.ip>
2026-05-29 09:58:49 +0200

    vite adjust

---
+++ src/ts/script.ts
@@ -0,0 +1,2 @@
+// На самому початку src/ts/script.ts додаєш:
+import '../scss/style.scss'
--- ts/script.ts
+++
@@ -1,2 +0,0 @@
-console.log('Hello from TypeScript!');
-console.log('This is a simple TypeScript script.');
