4eb38013eea3e52dfaa08cea7c8d553eb8c250a0
636 <jabko@macbookpro.speedport.ip>
2026-05-31 15:17:22 +0200

    project ready to continiue

--- src/ts/modules/register-form.ts
+++ src/ts/modules/register-form.ts
@@ -1,25 +1,43 @@
 export function initRegisterForm(): void {
-  const registerButton = document.getElementById("addBtn") as HTMLButtonElement;
-  const nameInput = document.getElementById("name") as HTMLInputElement;
-  const priceInput = document.getElementById("price") as HTMLInputElement;
-  const quantityInput = document.getElementById("qty") as HTMLInputElement;
+  const registerButton = document.getElementById(
+    "addBtn",
+  ) as HTMLButtonElement | null;
+  const nameInput = document.getElementById("name") as HTMLInputElement | null;
+  const priceInput = document.getElementById(
+    "price",
+  ) as HTMLInputElement | null;
+  const quantityInput = document.getElementById(
+    "qty",
+  ) as HTMLInputElement | null;
+
+  // Перевірка, чи всі елементи існують (Type Guard) якщо є null, то виводимо помилку і виходимо з функції
+  if (!registerButton || !nameInput || !priceInput || !quantityInput) {
+    console.error("Form elements not found!");
+    return;
+  }
 
   const validateInputs = (): void => {
-    const name = nameInput.value.trim();
-    const price = parseFloat(priceInput.value);
-    const quantity = parseInt(quantityInput.value);
+    const name: string = nameInput.value.trim();  // value це з обєкту nameInput, trim() видаляє пробіли з початку і кінця рядка
+    const price: number = parseFloat(priceInput.value); // parseFloat перетворює рядок в число з плаваючою точкою
+    const quantity: number = parseInt(quantityInput.value); // parseInt перетворює рядок в ціле число
 
-    if (name && price && quantity) {
-      registerButton.disabled = false;
-    } else {
-      registerButton.disabled = true;
-    }
-
+    // Логічний вираз замість if/else — коротше і зрозуміліше 
+    // тут ми перевіряємо, чи всі умови виконуються для активації кнопки реєстрації:
+    // якщо в середені все true, то disvabled буде false, якщо хоча б одна умова false, то disabled буде true
+    registerButton.disabled = !(
+      name &&
+      !isNaN(price) &&
+      !isNaN(quantity) &&
+      price > 0 &&
+      quantity > 0
+    );
   };
-    nameInput.addEventListener("input", validateInputs);
-    priceInput.addEventListener("input", validateInputs);
-    quantityInput.addEventListener("input", validateInputs);
 
-    // ВАЖЛИВО: Викликаємо відразу, щоб заблокувати кнопку при старті
+  // Реєструємо слухачі подій для кожного поля вводу, щоб викликати validateInputs при кожному зміненні
+  [nameInput, priceInput, quantityInput].forEach((input) : void => {
+    input.addEventListener("input", validateInputs);
+  });
+
+  // Перша валідація при завантаженні
   validateInputs();
 }
